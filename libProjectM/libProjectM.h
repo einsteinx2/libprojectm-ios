dbec65632c0884440d02819cdda6e16af2e91298
Ben Baron <ben@einsteinx2.com>
2017-07-02 13:06:52 +0200

    Initial Commit

---
+++ libProjectM/libProjectM.h
@@ -0,0 +1,13 @@
+//
+//  libProjectM.h
+//  libProjectM
+//
+//  Created by Benjamin Baron on 7/2/17.
+//  Copyright © 2017 Ben Baron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libProjectM : NSObject
+
+@end
---
+++ libProjectM/libProjectM.m
@@ -0,0 +1,13 @@
+//
+//  libProjectM.m
+//  libProjectM
+//
+//  Created by Benjamin Baron on 7/2/17.
+//  Copyright © 2017 Ben Baron. All rights reserved.
+//
+
+#import "libProjectM.h"
+
+@implementation libProjectM
+
+@end
